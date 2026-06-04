0934f2a5fd4f68e04e5ae838b8ddbfe77b2fc050
DragoKZs <sandinatoksanbaeva@gmail.com>
2026-06-04 11:43:01 +0500

    interface update4 fix5

--- apps/server/src/game/game.gateway.ts
+++ apps/server/src/game/game.gateway.ts
@@ -279,95 +279,95 @@ export class GameGateway {
           ) {
             room.timeLeft = 0;
           }
-        if (
-          room.timeLeft <=
-          0
-        ) {
-          clearInterval(
-            room.timer,
-          );
+          if (
+            room.timeLeft <=
+            0
+          ) {
+            clearInterval(
+              room.timer,
+            );
 
-          room.questionActive =
-            false;
+            room.questionActive =
+              false;
 
-          room.players.forEach(
-            (
-              player: any,
-            ) => {
-              const correct =
-                player.selectedAnswer ===
-                room
-                  .currentQuestion
-                  .correct;
-
-              if (
-                correct
-              ) {
+            room.players.forEach(
+              (
+                player: any,
+              ) => {
+                const correct =
+                  player.selectedAnswer ===
+                  room
+                    .currentQuestion
+                    .correct;
 
                 if (
-                  room.isLastChanceRound
+                  correct
                 ) {
-                  if (
-                    player.lives <= 0
-                  ) {
-                    player.lives = 1;
 
-                    player.eliminated =
-                      false;
-                  } else if (
-                    player.lives < 3
+                  if (
+                    room.isLastChanceRound
                   ) {
-                    player.lives++;
+                    if (
+                      player.lives <= 0
+                    ) {
+                      player.lives = 1;
+
+                      player.eliminated =
+                        false;
+                    } else if (
+                      player.lives < 3
+                    ) {
+                      player.lives++;
+                    } else {
+                      player.score += 50;
+                    }
                   } else {
-                    player.score += 50;
+                    player.score +=
+                      room.isFinalRound
+                        ? 300
+                        : room.isSpeedRound
+                          ? 200
+                          : 100;
+                  }
+
+                  player.streak++;
+
+                  if (
+                    player.streak >
+                    player.bestStreak
+                  ) {
+                    player.bestStreak =
+                      player.streak;
                   }
                 } else {
-                  player.score +=
-                    room.isFinalRound
-                      ? 300
-                      : room.isSpeedRound
-                        ? 200
-                        : 100;
+                  player.streak = 0;
+
+                  player.lives =
+                    Math.max(
+                      0,
+                      player.lives - 1,
+                    );
+
                 }
+              },
+            );
 
-                player.streak++;
+            this.server.to(
+              roomCode,
+            ).emit(
+              'questionEnded',
+              {
+                correct:
+                  room
+                    .currentQuestion
+                    .correct,
+              },
+            );
 
-                if (
-                  player.streak >
-                  player.bestStreak
-                ) {
-                  player.bestStreak =
-                    player.streak;
-                }
-              } else {
-                player.streak = 0;
-
-                player.lives =
-                  Math.max(
-                    0,
-                    player.lives - 1,
-                  );
-
-              }
-            },
-          );
-
-          this.server.to(
-            roomCode,
-          ).emit(
-            'questionEnded',
-            {
-              correct:
-                room
-                  .currentQuestion
-                  .correct,
-            },
-          );
-
-          this.emitPlayers(
-            roomCode,
-          );
-        }
+            this.emitPlayers(
+              roomCode,
+            );
+          }
         }, 1000);
     };
 
@@ -408,7 +408,7 @@ export class GameGateway {
             },
           );
 
-      startTimer();
+        startTimer();
       }, 2000);
     } else {
       this.server.to(
@@ -435,141 +435,11 @@ export class GameGateway {
       );
       startTimer();
     }
-
-    this.server.to(roomCode).emit(
-      'timerUpdate',
-      {
-        timeLeft:
-          room.timeLeft,
-      },
-    );
-
-    room.timer =
-      setInterval(() => {
-        if (
-          room.paused
-        )
-          return;
-
-        room.timeLeft--;
-
-        this.server.to(
-          roomCode,
-        ).emit(
-          'timerUpdate',
-          {
-            timeLeft:
-              room.timeLeft,
-          },
-        );
-
-        const everyoneAnswered =
-          room.players.every(
-            (p: any) =>
-              p.hasAnswered,
-          );
-
-        if (
-          everyoneAnswered
-        ) {
-          room.timeLeft = 0;
-        }
-
-        if (
-          room.timeLeft <=
-          0
-        ) {
-          clearInterval(
-            room.timer,
-          );
-
-          room.questionActive =
-            false;
-
-          room.players.forEach(
-            (
-              player: any,
-            ) => {
-              const correct =
-                player.selectedAnswer ===
-                room
-                  .currentQuestion
-                  .correct;
-
-              if (
-                correct
-              ) {
-
-                if (
-                  room.isLastChanceRound
-                ) {
-                  if (
-                    player.lives <= 0
-                  ) {
-                    player.lives = 1;
-
-                    player.eliminated =
-                      false;
-                  } else if (
-                    player.lives < 3
-                  ) {
-                    player.lives++;
-                  } else {
-                    player.score += 50;
-                  }
-                } else {
-                  player.score +=
-                    room.isFinalRound
-                      ? 300
-                      : room.isSpeedRound
-                        ? 200
-                        : 100;
-                }
-
-                player.streak++;
-
-                if (
-                  player.streak >
-                  player.bestStreak
-                ) {
-                  player.bestStreak =
-                    player.streak;
-                }
-              } else {
-                player.streak = 0;
-
-                player.lives =
-                  Math.max(
-                    0,
-                    player.lives - 1,
-                  );
-
-              }
-            },
-          );
-
-          this.server.to(
-            roomCode,
-          ).emit(
-            'questionEnded',
-            {
-              correct:
-                room
-                  .currentQuestion
-                  .correct,
-            },
-          );
-
-          this.emitPlayers(
-            roomCode,
-          );
-        }
-      }, 1000);
   }
-
+  
   private startReactionGame(
-    roomCode: string,
-  ) {
+      roomCode: string,
+    ) {
     const room =
       this.getRoom(roomCode);
 
